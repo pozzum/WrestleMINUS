60809280a96930e104e5891f35086ffbd5b76662
Pozzum <Pozzumspityt@gmail.com>
2019-11-29 10:59:01 -0500

    Cak View Improvement

--- WrestleMINUS/MainForm.vb
+++ WrestleMINUS/MainForm.vb
@@ -6627,14 +6627,42 @@ Public Class MainForm
         'Folder Views
         'DataGridCAkFolderView
         Dim ContainedFolderList As List(Of PackageHandlers.CakFileHandler.PackFolderEntry) = TempCakFile.GetFolderInformation()
+        Dim CloneRow As DataGridViewRow = ClearandGetClone(DataGridCAkFolderView)
+        Dim WorkingCollection As List(Of DataGridViewRow) = New List(Of DataGridViewRow)
+        ProgressBar1.Value = 0
+        ProgressBar1.Maximum = ContainedFolderList.Count - 1
         For i As Integer = 0 To ContainedFolderList.Count - 1
-            DataGridCAkFolderView.Rows.Add(i, ContainedFolderList(i).Name, ContainedFolderList(i).Unk, Hex(ContainedFolderList(i).Hash), ContainedFolderList(i).FolderIndices.Count, ContainedFolderList(i).FileIndices.Count)
+            Dim TempGridRow As DataGridViewRow = CloneRow.Clone()
+            TempGridRow.Cells(DataGridCAkFolderView.Columns.IndexOf(CAkFolderIndexColumn)).Value = i
+            TempGridRow.Cells(DataGridCAkFolderView.Columns.IndexOf(CAkFolderNameColumn)).Value = ContainedFolderList(i).Name
+            TempGridRow.Cells(DataGridCAkFolderView.Columns.IndexOf(CAkFolderUnkColumn)).Value = ContainedFolderList(i).Unk
+            TempGridRow.Cells(DataGridCAkFolderView.Columns.IndexOf(CAkFolderHashColumn)).Value = Hex(ContainedFolderList(i).Hash)
+            TempGridRow.Cells(DataGridCAkFolderView.Columns.IndexOf(CAkFolderFolderCountColumn)).Value = ContainedFolderList(i).FolderIndices.Count
+            TempGridRow.Cells(DataGridCAkFolderView.Columns.IndexOf(CAkFolderFileCountColumn)).Value = ContainedFolderList(i).FileIndices.Count
+            WorkingCollection.Add(TempGridRow)
+            ProgressBar1.Value = i
         Next
+        DataGridCAkFolderView.Rows.AddRange(WorkingCollection.ToArray())
         'DataGridCAkFilesView
         Dim ContainedFileList As List(Of PackageHandlers.CakFileHandler.PackFileEntry) = TempCakFile.GetFileInformation()
+        Dim CloneFileRow As DataGridViewRow = ClearandGetClone(DataGridCAkFilesView)
+        Dim WorkingFileCollection As List(Of DataGridViewRow) = New List(Of DataGridViewRow)
+        ProgressBar1.Value = 0
+        ProgressBar1.Maximum = ContainedFileList.Count - 1
         For i As Integer = 0 To ContainedFileList.Count - 1
-            DataGridCAkFilesView.Rows.Add(i, ContainedFileList(i).Name, ContainedFileList(i).FolderIndex, Hex(ContainedFileList(i).Hash), ContainedFileList(i).Offset, ContainedFileList(i).Size, ContainedFileList(i).Crc, ContainedFileList(i).Type)
+            Dim TempGridRow As DataGridViewRow = CloneFileRow.Clone()
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileIndexColumn)).Value = i
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileNameColumn)).Value = ContainedFileList(i).Name
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileUnkColumn)).Value = ContainedFileList(i).FolderIndex
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileHashColumn)).Value = Hex(ContainedFileList(i).Hash)
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileOffsetColumn)).Value = ContainedFileList(i).Offset
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileSizeColumn)).Value = ContainedFileList(i).Size
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileCrcColumn)).Value = ContainedFileList(i).Crc
+            TempGridRow.Cells(DataGridCAkFilesView.Columns.IndexOf(CAkFileTypeColumn)).Value = ContainedFileList(i).Type
+            WorkingFileCollection.Add(TempGridRow)
+            ProgressBar1.Value = i
         Next
+        DataGridCAkFilesView.Rows.AddRange(WorkingFileCollection.ToArray())
     End Sub
 
 #End Region
